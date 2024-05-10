f1273f3718240b1f0b4da02a05871c35009ac8e2
windybird <windybird@qq.com>
2024-05-10 11:10:42 +0800

    Create cmcc_rax3000m-256m.sh

---
+++ cmcc_rax3000m-256m.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.13/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/10.10.10.13/g' package/base-files/files/bin/config_generate
+
+# change the login password
+
+sed -i 's/root::0:0:99999:7:::/root:$1$iZM.01X5$xfeRwcqbhN\/60\/2SUPwDc\/:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+
+#修改wifi名称（mtwifi-cfg）
+sed -i 's/ImmortalWrt-2.4G/XYKJ/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ImmortalWrt-5G/XYKJ/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+# 添加openclash
+rm -rf package/feeds/luci/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+mkdir package/openclash
+cd package/openclash
+git init
+git remote add origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+mv luci-app-openclash ../
+cd ../../
+rm -rf package/openclash
+
+#删除冲突的软件包
+#rm -rf ./package/istore
+#rm -rf ./feeds/kenzo/luci-app-quickstart
+#rm -rf ./feeds/kenzo/luci-app-store
+#rm -rf ./feeds/kenzo/luci-lib-taskd
+
+#修改闪存为256M版本(这是针对原厂128闪存来的，但又要编译256M固件来的）
+#sed -i 's/<0x580000 0x7200000>/<0x580000 0xee00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
+#sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
