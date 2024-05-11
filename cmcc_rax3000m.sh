baccd71b9319c59167d3c5d1a9d665de945721c9
windybird <windybird@qq.com>
2024-05-12 07:09:00 +0800

    Update cmcc_rax3000m.sh

--- cmcc_rax3000m.sh
+++ cmcc_rax3000m.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改IP(B类地址)和主机名
-sed -i 's/192.168.1.1/172.16.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/172.16.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.240.0.0/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/HOME/g' package/base-files/files/bin/config_generate
 
