b6484aa602d1e2eab14885ff3435c940169b0722
ezxykj <ezxykj@qq.com>
2024-08-02 14:32:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -56,3 +56,7 @@ mv files/luci-app-frpc feeds/luci/applications/
 chmod -R 755 feeds/luci/applications/luci-app-frpc/
 sed -i '/PKG_VERSION:=/c\PKG_VERSION:=0.58.1' feeds/packages/net/frp/Makefile
 sed -i '/PKG_HASH:=/c\PKG_HASH:=c6eabdc2bf39bdb4a7ab7794a4b2ad94be5e0cab50b6cc540a6431e61208b1e6' feeds/packages/net/frp/Makefile
+
+# 修改appfilter
+rm -rf feeds/luci/applications/luci-app-appfilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
