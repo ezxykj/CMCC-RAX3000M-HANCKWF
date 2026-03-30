25df30ca9a3d5efdbb5c508a8cffa8fc19993678
ezxykj <ezxykj@qq.com>
2026-03-30 12:28:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -115,23 +115,23 @@ git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-time
 sed -i '/$(eval $(call BuildPackage,$(PKG_NAME)))/s/^/#/' package/luci-app-timecontrol/luci-app-timecontrol/Makefile
 
 # h3c_nx30_pro_112m分区
-sed -i '/^TARGET_DEVICES += cmcc_rax3000m-emmc$/a \
+sed -i '/^TARGET_DEVICES += h3c_nx30pro$/a\
 \
-define Device/h3c_nx30pro-112m \
-  DEVICE_VENDOR := H3C \
-  DEVICE_MODEL := NX30PRO (with 112M ubi) \
-  DEVICE_DTS := mt7981-h3c-nx30pro-112m \
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek \
-  SUPPORTED_DEVICES := h3c,nx30pro \
-  UBINIZE_OPTS := -E 5 \
-  BLOCKSIZE := 128k \
-  PAGESIZE := 2048 \
-  IMAGE_SIZE := 114688k \
-  KERNEL_IN_UBI := 1 \
-  IMAGES += factory.bin \
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE) \
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata \
-endef \
+define Device/h3c_nx30pro-112m\
+  DEVICE_VENDOR := H3C\
+  DEVICE_MODEL := NX30PRO (with 112M ubi)\
+  DEVICE_DTS := mt7981-h3c-nx30pro-112m\
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek\
+  SUPPORTED_DEVICES := h3c,nx30pro\
+  UBINIZE_OPTS := -E 5\
+  BLOCKSIZE := 128k\
+  PAGESIZE := 2048\
+  IMAGE_SIZE := 114688k\
+  KERNEL_IN_UBI := 1\
+  IMAGES += factory.bin\
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)\
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata\
+endef\
 TARGET_DEVICES += h3c_nx30pro-112m
 ' target/linux/mediatek/image/mt7981.mk
 mv files/dts/mt7981-h3c-nx30pro-112m.dts target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/
