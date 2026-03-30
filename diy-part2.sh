eff8b47b01d820d9562acb869c6cad21b09b3123
ezxykj <ezxykj@qq.com>
2026-03-30 11:05:04 +0800

    Add h3c_nx30pro-112m device support
    Added support for h3c_nx30pro-112m device with updated specifications.

--- diy-part2.sh
+++ diy-part2.sh
@@ -115,9 +115,22 @@ git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-time
 sed -i '/$(eval $(call BuildPackage,$(PKG_NAME)))/s/^/#/' package/luci-app-timecontrol/luci-app-timecontrol/Makefile
 
 # h3c_nx30_pro_112m分区
-sed -i '/^define Device\/h3c_nx30pro$/,/^endef$/{s/IMAGE_SIZE := 65536k/IMAGE_SIZE := 114688k/}' target/linux/mediatek/image/mt7981.mk
-sed -i 's/define Device\/h3c_nx30pro/define Device\/h3c_nx30pro-112m/' target/linux/mediatek/image/mt7981.mk
-sed -i 's/TARGET_DEVICES += h3c_nx30pro/TARGET_DEVICES += h3c_nx30pro-112m/' target/linux/mediatek/image/mt7981.mk
-sed -i 's/model = "H3C NX30PRO"/model = "H3C NX30PRO 112M"/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-h3c-nx30pro.dts
-sed -i 's/reg = <0x580000 0x4000000>/reg = <0x580000 0x7000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-h3c-nx30pro.dts
-sed -i '94,114d' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-h3c-nx30pro.dts
+sed -i '/^TARGET_DEVICES += cmcc_rax3000m-emmc$/a \
+\
+define Device/h3c_nx30pro-112m \
+  DEVICE_VENDOR := H3C \
+  DEVICE_MODEL := NX30PRO (with 112M ubi) \
+  DEVICE_DTS := mt7981-h3c-nx30pro-112m \
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek \
+  SUPPORTED_DEVICES := h3c,nx30pro \
+  UBINIZE_OPTS := -E 5 \
+  BLOCKSIZE := 128k \
+  PAGESIZE := 2048 \
+  IMAGE_SIZE := 114688k \
+  KERNEL_IN_UBI := 1 \
+  IMAGES += factory.bin \
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE) \
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata \
+endef \
+TARGET_DEVICES += h3c_nx30pro-112m
+' target/linux/mediatek/image/mt7981.mk
